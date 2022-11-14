2a364f10cb91b34560aabe6b36f74afcc17e41ca
Streltsov_Mike <streltsovmike@yandex.ru>
2022-11-14 20:27:09 +0300

    add quick sort

---
+++ quick_sort.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+    public class quick_sort {
+        public static void main(String[] args) {
+            int[] array = {-10, 3, 17, 100, 55, -12, -100, 1, 1, 1, 4, 12 };
+            System.out.println("Было");
+            System.out.println(Arrays.toString(array));
+
+            int low = 0;
+            int high = array.length - 1;
+
+            quickSort(array, low, high);
+            System.out.println("Стало");
+        System.out.println(Arrays.toString(array));
+        }
+
+        public static void quickSort(int[] array, int low, int high) {
+            if (array.length == 0)
+                return;
+
+            if (low >= high)
+                return;
+
+            int middle = low + (high - low) / 2;
+            int opora = array[middle];
+
+            int i = low, j = high;
+            while (i <= j) {
+                while (array[i] < opora) {
+                    i++;
+                }
+
+                while (array[j] > opora) {
+                    j--;
+                }
+
+                if (i <= j) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+        }
+
+            if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+                quickSort(array, i, high);
+        }   
+}
